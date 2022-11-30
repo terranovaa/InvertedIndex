845aba8f767a00394f791f3b164f447a3e854901
Jacopo Cecchetti <j.cecchetti23@gmail.com>
2022-11-30 09:34:52 +0100

    Changed read() to readLine()

--- src/main/java/it/unipi/Indexer.java
+++ src/main/java/it/unipi/Indexer.java
@@ -3,10 +3,7 @@ package it.unipi;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -31,26 +28,15 @@ public class Indexer {
         File file = new File(collectionPath);
         final TarArchiveInputStream tarArchiveInputStream = new TarArchiveInputStream(new FileInputStream(file));
         TarArchiveEntry tarArchiveEntry = tarArchiveInputStream.getNextTarEntry();
+        BufferedReader bufferedReader;
         if (tarArchiveEntry != null) {
-            byte[] bytes = new byte[2048]; // Random number
-            // contains the last document of the block which is not whole
-            String lastDocument = "";
-            while (tarArchiveInputStream.read(bytes) != -1) {
-                // UTF8 or ASCII?
-                String test = lastDocument + new String(bytes, StandardCharsets.UTF_8);
-                String[] lines = test.split("\n");
-                Iterator<String> linesIterator = Arrays.stream(lines).iterator();
-                while (linesIterator.hasNext()) {
-                    String line = linesIterator.next();
-                    // Saving the partial document
-                    if (!linesIterator.hasNext()) {
-                        lastDocument = line;
-                        break;
-                    }
-                    String doc_id = line.substring(0, line.indexOf("\t"));
-                    String document = line.substring(line.indexOf("\t") + 1);
-                    // TODO split into tokens and implement SPIMI
-                }
+            // UTF8 or ASCII?
+            bufferedReader = new BufferedReader(new InputStreamReader(tarArchiveInputStream, StandardCharsets.UTF_8));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String doc_no = line.substring(0, line.indexOf("\t"));
+                String document = line.substring(line.indexOf("\t") + 1);
+                // TODO split into tokens and implement SPIMI
             }
         }
     }
